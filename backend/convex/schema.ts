bba3f88de601047f3af1c161395cb99c9d649ab8
joatuo <tuovinen@chalmers.se>
2025-09-18 13:28:15 +0200

    Updated database schema

--- backend/convex/schema.ts
+++ backend/convex/schema.ts
@@ -1,32 +1,18 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-enum UserRole {
-  Restaurant = "RESTAURANT",
-  NGO = "NGO",
-  Individual = "INDIVIDUAL",
-  Store = "STORE",
-  Charity = "CHARITY"
-}
-
-const roleValidator = v.union(
-  v.literal(UserRole.Restaurant),
-  v.literal(UserRole.NGO),
-  v.literal(UserRole.Individual),
-  v.literal(UserRole.Store),
-  v.literal(UserRole.Charity)
-)
-
 enum DonationStatus {
   Available = "AVAILABE",
   Claimed = "CLAIMED",
-  PickedUp = "PICKEDUP"
+  PickedUp = "PICKEDUP",
+  Expired = "EXPIRED"
 }
 
 const donationStatusValidator = v.union(
   v.literal(DonationStatus.Available),
   v.literal(DonationStatus.Claimed),
-  v.literal(DonationStatus.PickedUp)
+  v.literal(DonationStatus.PickedUp),
+  v.literal(DonationStatus.Expired)
 )
 
 enum ClaimedStatus {
@@ -41,25 +27,46 @@ const claimedStatusValidator = v.union(
 
 export default defineSchema({
   users: defineTable({
-    name: v.string(),
-    email: v.string(),
-    role: roleValidator,
+    first_name: v.string(),
+    last_name: v.string(),
     address: v.string(),
-    isVerified: v.boolean(),
+    phone: v.string(),
+  }),
+  donors: defineTable({
+    user_id: v.id("users"),
+    business_name: v.string(),
+    business_email: v.string(),
+    business_phone: v.string(),
+    verified: v.boolean(),
+  }),
+  recievers: defineTable({
+    individuals_id: v.optional(v.id("individuals")),
+    volunteer_id: v.optional(v.id("volunteers")),
+  }),
+  volunteers: defineTable({
+    user_id: v.id("users"),
+    volunteer_name: v.string(),
+    contact_phone: v.string(),
+    contact_email: v.string(),
+    verified: v.boolean(),
+  }),
+  individuals: defineTable({
+    user_id: v.id("users"),
+    food_allergy: v.string(),
   }),
   donations: defineTable({
-    donorId: v.id("users"),
+    donor_id: v.id("donors"),
     title: v.string(),
     description: v.string(),
     quantity: v.int64(),
     pickup_window_start: v.string(),
     pickup_window_end: v.string(),
-    location: v.string(),
     status: donationStatusValidator,
   }),
   claims: defineTable({
-    donationId: v.id("donations"),
-    claimerId: v.id("users"),
+    donation_id: v.id("donations"),
+    claimer_id: v.id("recievers"),
+    amount: v.int64(),
     status: claimedStatusValidator,
   }),
 });
